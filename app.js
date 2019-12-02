21baf6eb1c3080684c60a1c6b3316b8d61843195
Mike Hu <mikehu@Mikes-MacBook-Pro.local>
2019-12-02 15:33:25 -0500

    connecting to Mongodb using mongoose

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express();
 const expressLayout = require("express-ejs-layouts");
+const mongoose = require('mongoose');
 
+
+//DB config
+const db = require('./config/keys').MongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Mongodb Connected...'))
+  .catch(err => console.log(err)
+);
+
+//EJS
 app.use(expressLayout);
 app.set("view engine", "ejs");
 
+//Serving static files in Express
 app.use(express.static("public"));
 app.use(express.static(__dirname + "/public"));
 
+//Routing to home
 app.get("/", function(req, res) {
   res.render("home");
 });
 
+//Routing to library and about page
 app.get("/library", (req, res) => res.render("library"));
-
 app.get("/about", (req, res) => res.render("about"));
 
+//Routing to PageNotFound
 app.get("*", function(req, res) {
   res.render("PageNotFound");
 });
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MongoURI: 'mongodb+srv://yhu09:rateyourmeme@cluster0-73tka.mongodb.net/test?retryWrites=true&w=majority'
+}
