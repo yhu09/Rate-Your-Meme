95a728eff326f704cc4fcca42f449a30ac9b06ee
Ryan Hoff <rhoff20@gmail.com>
2019-12-11 00:10:49 -0500

    get meme server side is now working

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  MongoURI: 'mongodb+srv://yhu09:rateyourmeme@cluster0-73tka.mongodb.net/test?retryWrites=true&w=majority'
+  MongoURI: 'mongodb+srv://admin:91wa0EcHAtoTqeU3@comp20-ohns9.mongodb.net/test?retryWrites=true&w=majority'
 }
