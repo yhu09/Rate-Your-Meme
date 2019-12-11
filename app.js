7b70b0c0532b37a90dda49baf04bc4d60b2e10aa
Mike Hu <mikehu@Mikes-MacBook-Pro.local>
2019-12-10 21:38:57 -0500

    fixing deplyment problem adding /home

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(express.static("public"));
 app.use(express.static(__dirname + "/public"));
 
 //Routing to home
-app.get("/", function(req, res) {
+app.get("/home", function(req, res) {
   res.render("home");
 });
 
